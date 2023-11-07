46b47ce172b14cf2df733f4e21f6ef3a0f117b39
HaseebAhmad522 <haseeb.bhp@gmail.com>
2023-11-07 17:36:30 +0500

    latest code with pagination class

--- home/api/v1/viewsets.py
+++ home/api/v1/viewsets.py
@@ -7,6 +7,7 @@ from home.models import Watch
 from .serializers import WatchSerializer, WatchTitleFilter, WatchFilter
 from django.db import models 
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 
 
 class CSVUploadView(APIView):
@@ -89,24 +90,57 @@ class CSVUploadView(APIView):
         else:
             return Response({'error': 'File must be a CSV'}, status=status.HTTP_400_BAD_REQUEST)
 
-class FilterView(APIView):
+class FilterView(APIView, PageNumberPagination):
 
     def get(self, request):
         title = request.query_params.get('title', '')  # Get the title from query parameters
         watches = Watch.objects.filter(title__icontains=title)
+        page = self.paginate_queryset(watches, request)
+        if page is not None:
+          serializer = WatchSerializer(page, many=True)
+          return self.get_paginated_response(serializer.data)
+        
         serializer = WatchSerializer(watches, many=True)
         return Response(serializer.data)
     
-class WatchFilter(APIView):
+class WatchFilter(APIView, PageNumberPagination):
+    # pagination_class = PageNumberPagination
 
     def get(self, request):
-        min_price = request.query_params.get('min_price', '')
-        max_price = request.query_params.get('max_price', '')
+        min_price = request.query_params.get('min_price')
+        max_price = request.query_params.get('max_price')
+
+        # Check if both min_price and max_price are provided
+        if not min_price or not max_price:
+            return Response({'error': 'Both min_price and max_price are required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            min_price = float(min_price)
+            max_price = float(max_price)
+        except ValueError:
+            return Response({'error': 'Invalid price format, expected numbers'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if min_price >= max_price:
+            return Response({'error': 'min_price should be less than max_price'}, status=status.HTTP_400_BAD_REQUEST)
+
         watches = Watch.objects.filter(price__range=(min_price, max_price))
+        page = self.paginate_queryset(watches, request)
+        if page is not None:
+            serializer = WatchSerializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        
         serializer = WatchSerializer(watches, many=True)
         return Response(serializer.data)
+
+    # def get(self, request):
+    #     min_price = request.query_params.get('min_price', '')
+    #     max_price = request.query_params.get('max_price', '')
+    #     watches = Watch.objects.filter(price__range=(min_price, max_price))
+    #     serializer = WatchSerializer(watches, many=True)
+    #     return Response(serializer.data)
     
-class WatchListAPIView(APIView):
+class WatchListAPIView(APIView, PageNumberPagination):
+    
     def get(self, request):
         # Get the query parameters from the request
         query_params = request.query_params
@@ -125,13 +159,16 @@ class WatchListAPIView(APIView):
             serializer = WatchSerializer(queryset, many=True)
 
         # Apply pagination
-        page = int(query_params.get('page', 1))
-        limit = int(query_params.get('limit', 100))
-        start = (page - 1) * limit
-        end = page * limit
-        paginated_data = serializer.data[start:end]
+        page = self.paginate_queryset(serializer.data, request)
+        if page is not None:
+            return self.get_paginated_response(page)
+        # page = int(query_params.get('page', 1))
+        # limit = int(query_params.get('limit', 100))
+        # start = (page - 1) * limit
+        # end = page * limit
+        # paginated_data = serializer.data[start:end]
 
-        return Response(paginated_data)
+        return Response(serializer.data)
 
 
     # def get(self, request):
--- watches/settings.py
+++ watches/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 CORS_ALLOW_ALL_ORIGINS = True  # new
 
@@ -131,3 +132,10 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+}
